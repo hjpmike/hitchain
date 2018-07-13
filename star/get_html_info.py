942d41bab2f0aee6c68949040b97c7954d25edb8
whystar <starleelzx@163.com>
2018-07-13 20:26:11 +0800

    done the everydays' loop logic

--- star/get_html_info.py
+++ star/get_html_info.py
@@ -3,6 +3,18 @@
 created by starlee @ 2018-07-13 15:00
 for fetching raw infos from html
 '''
+import time
+import logging
+ 
+logger = logging.getLogger()
+hdlr = logging.FileHandler("log/get_html_info.log")
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.NOTSET)
+
+
+INTERVAL_TIME = 6 #in seconds
 
 def readPrjLists():
 	prjs = []
@@ -11,9 +23,28 @@ def readPrjLists():
 			prjls = prj_line.split("\t")
 			prjs.append(prjls[0])
 	return prjs
-			
+
+
+def fetchHtmlInfo(prj):
+	pass
+
 def main():
-	prjs = readPrjLists()
+	while True:
+
+		logger.info("start another round of work")
+		# 爬完历史信息后，每个一天更新一次
+		start_time = time.time()
+
+		prjs = readPrjLists()
+		for prj in prjs:
+			fetchHtmlInfo(prj)
+		
+		end_time = time.time()
+		work_time = end_time - start_time
+		if work_time < INTERVAL_TIME:
+			logger.info("not enough interval, sleep a while")
+			time.sleep(INTERVAL_TIME - work_time)
+
 
 if __name__ == '__main__':
 	main()

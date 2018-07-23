761a752e769354866ce3ece0eae4e3e36c84860c
whystar <starleelzx@163.com>
2018-07-23 12:54:37 +0800

    udpate db config

--- metric_collecting/star/config.py
+++ metric_collecting/star/config.py
@@ -1,10 +1,10 @@
 #coding:utf-8
 config = {
 	"db_host" : "localhost",
-	"db_name" : "hcc",
-	"db_user" : "hcc_user",
-	"db_passwd" : "hcc_passwd",
-	"html_fetch_interval" : 300, #in second
-	"json_fetch_interval" : 300, #in second
+	"db_name" : "ossean_coin_rank",
+	"db_user" : "root",
+	"db_passwd" : "ossean@rank@123",
+	"html_fetch_interval" : 86400, #in second
+	"json_fetch_interval" : 86400, #in second
 	'db_conn_pool_size': 5
 }

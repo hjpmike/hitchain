159812f5cd52e8f5334214fdd61faa910568609f
whystar <starleelzx@163.com>
2018-07-24 19:08:07 +0800

    update db setting

--- metric_computing/star/config.py
+++ metric_computing/star/config.py
@@ -3,7 +3,7 @@ config = {
 	"db_host" : "localhost",
 	"db_name" : "ossean_coin_rank",
 	"db_user" : "root",
-	"db_passwd" : "ossean@rank@123",
+	"db_passwd" : "password",
 	"metric_compute_interval" : 86400, #in second
 	'db_conn_pool_size': 5,
 	'examine_window': 30 #in day

32cddaf12e1c23cc4550e11142780fe28745b8be
whystar <starleelzx@163.com>
2018-07-24 20:08:52 +0800

    minor

--- metric_computing/star/metrics.py
+++ metric_computing/star/metrics.py
@@ -425,7 +425,7 @@ def computeScore():
 						(repo,dateTime),(0,0)))
 		M3[repo] = _my_sum(dbop.select_one("select repair_ratio,repair_time from quality_sub where repo_id=%s and computed_at<=%s order by id limit 1",
 						(repo,dateTime),(0,0)))
-		M4[repo] = _my_sum(dbop.select_one("selecg  ccr,ngr,tbr from team_health where repo_id=%s and computed_at<=%s order by id limit 1",
+		M4[repo] = _my_sum(dbop.select_one("select  ccr,ngr,tbr from team_health where repo_id=%s and computed_at<=%s order by id limit 1",
 						(repo,dateTime),(0,0)))
 		M5[repo] = _my_sum(dbop.select_one("select  ,dev,rel from dev_actv where repo_id=%s and computed_at<=%s order by id limit 1",
 						(repo,dateTime),(0,0)))
